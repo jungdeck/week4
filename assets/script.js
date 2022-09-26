9f73bd725559d8fdad822e059d20ae88d7b9b5b8
jungdeck <jdeck7021@gmail.com>
2022-09-26 16:43:00 -0400

    fourth assignment v1

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+/*
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+Then each question a small border box with CORRECT or INCORRECT slides infront of the screen before the next question.
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score
+*/
+
+// 1. timer with a countdown of 500000 ms
+// 2. timer for each question of 20000 ms
+//     a. if a question is correct + 5000ms 
+//     b. if a question is wrong - 5000ms 
+// 3. Question prompts
+//     a. if a question is correct + correct animation plays 
+//     b. if a question is wrong + wrong animation plays
+// 4. form with user input +Initials + score of the quiz 
+// 5. return to the start page
+// 6. local storage of +initials + score of the quiz 
